59fad1bbaa49a3135d9e3d94c88f9554c0b29c17
Yanik Beauileu <yanikbeaulieu@hotmail.com>
2024-10-12 18:45:33 -0400

    conversion to ejs

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const PORT = 3000;
 const RESULTS_FILE = 'results.csv';
 const SCAN_COMMAND = 'ipscan -f:range 192.168.1.1 192.168.1.254 -o results.csv -s -q';
 
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.use(express.static('public'));
 
 app.get('/scan', (req, res) => {
@@ -28,6 +34,20 @@ app.get('/scan', (req, res) => {
         }
     });
 });
+
+
+
+
+// Route for Home Page
+app.get('/', (req, res) => {
+    res.render('index'); // Renders 'views/index.ejs'
+});
+
+// Route for gCoder Page
+app.get('/gCoder', (req, res) => {
+    res.render('gCoder'); // Renders 'views/gCoder.ejs'
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
