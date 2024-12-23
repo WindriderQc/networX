427dab45e34596cba8100e4684536dab3f653db2
Yanik Beaulieu <yanikbeaulieu@hotmail.com>
2024-12-22 19:50:56 -0500

    linuxsetup updated

--- public/scripts/LinuxSetup.sh
+++ public/scripts/LinuxSetup.sh
@@ -16,35 +16,30 @@ node -v
 # Install PM2
 sudo npm install pm2@latest -g
 pm2 startup systemd
+echo "PM2 has been installed and configured. Remember to 'pm2 save' after adding your processes." 
+# Example of adding a process 
+# pm2 start your-application.js 
+# Save the process list and environment 
+pm2 save
+
 
 # required for platformIO (VS Code) to install properly
 sudo apt install python3-venv -y
 
-# Install VS Code   
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code 
-# or code-insiders
-
-sudo apt install git -y
-
-
-
-
-sudo apt autoremove --purge libreoffice* thunderbird  -y
-
-
-#######     USER NAME REQUIRED HERE   et dépends de ou le script est roulé   bad..  ###########     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  <---    <---
-rm /home/yb/Desktop/packages.microsoft.gpg
-
+# Add the Visual Studio Code repository 
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null 
+# Update the package list and install Visual Studio Code sudo 
+apt update sudo apt install code -y 
+# Verify installation 
+code --version
+echo "VS Code has been installed successfully!"
 
 #Improve Application Startup Speed With Preload in Ubuntu  
 sudo apt install preload
 
+sudo apt install git -y
 
+sudo apt autoremove --purge libreoffice* thunderbird  -y
 
-sudo apt install nemo-preview nemo-terminal nemo-image-converter nemo-share nemo-media-columns nemo-audio-tab nemo-compare 
+#sudo apt install nemo-preview nemo-terminal nemo-image-converter nemo-share nemo-media-columns nemo-audio-tab nemo-compare 
